9afa47df214316f8292ae06d55d0c1b8736de2d5
ComfortBlanket <30023105+comfortblanket@users.noreply.github.com>
2024-06-04 12:47:54 -0400

    Restructure with separate processImages(); add IntersectionObserver

--- src/content/mtg_collected_copies.js
+++ src/content/mtg_collected_copies.js
@@ -12,111 +12,116 @@ chrome.storage.local.get('cardQuantities').then(
         // <img> nodes we've already processed
         let processedImages = new Set();
     
+        let processImage = function(img) {
+
+            // Skip images we've already processed
+            if (processedImages.has(img)) {
+                return;
+            }
+            
+            // Find the parent <div> of the <img>, to deal with split cards
+            let parentDiv = img.parentNode;
+            while (parentDiv && parentDiv.nodeName !== 'DIV') {
+                parentDiv = parentDiv.parentNode;
+            }
+
+            // If the parent <div> contains other processed imgs, skip 
+            // this <img>. This is to deal with split cards and 
+            // partner commanders on EDHREC.
+            if (parentDiv) {
+                let foundOtherImg = false;
+                let otherImgs = parentDiv.getElementsByTagName('img');
+
+                for (let otherImg of otherImgs) {
+                    if (processedImages.has(otherImg)) {
+                        foundOtherImg = true;
+                        break;
+                    }
+                }
+
+                if (foundOtherImg) {
+                    return;
+                }
+            } else {
+                // Quick-fix: If we didn't find a parent <div>, just use the <img>'s parent
+                parentDiv = img.parentNode;
+            }
+
+            // Make a note that we've now processed this <img>
+            processedImages.add(img);
+
+            // Clean up the card name from the image's alt text
+            let cardName = img.alt
+                .toLowerCase()
+                .replace(/ [-a-z0-9]+$/, '')  // Replace trailing combinations of numbers, dashes, and letters
+                .replace('product image for', '')  // Strip out "Product Image for"
+                .replace('- full art', '')  // Strip out "- Full Art"
+                .replace(/-\d+$/, '')  // Strip "-0", "-2", etc. from end
+                .replace(/\(.*?\)/g, '')  // Strip out parentheses
+                .replace(/\[.*?\]/g, '')  // Strip out square brackets
+                .trim();
+            
+            let useCardNames = [];
+            if (cardQuantities.hasOwnProperty(cardName)) {
+                useCardNames.push(cardName);
+            }
+
+            // Faces of split cards -- actually for partner commanders on EDHREC
+            if (cardName.includes('//')) {
+                for (let splitPart of cardName.split('//')) {
+                    splitPart = splitPart.trim();
+                    if (cardQuantities.hasOwnProperty(splitPart)) {
+                        useCardNames.push(splitPart);
+                    } else {
+                        useCardNames.push(null);
+                    }
+                }
+            }
+
+            let quantityString = 'Collected: ';
+            if (useCardNames.some(cardName => cardName !== null)) {
+                quantityString += useCardNames.map(cardName => cardQuantities[cardName] ? cardQuantities[cardName] : 0).join(' // ');
+            } else {
+                useCardNames = [];
+            }
+            
+            if (useCardNames.length > 0) {
+                // We have a copy of this card, so add a label to the image
+                // console.log('HAVE ' + cardName + ' (' + cardQuantities[cardName] + ')');
+                let quantityLabel = document.createElement('div');
+                quantityLabel.textContent = quantityString;
+                quantityLabel.style.position = 'absolute';
+                quantityLabel.style.right = '0';
+                quantityLabel.style.top = '50%';
+                quantityLabel.style.transform = 'translateY(-100%)';
+                quantityLabel.style.backgroundColor = 'gray';
+                quantityLabel.style.color = 'white';
+                quantityLabel.style.fontWeight = 'bold';
+                quantityLabel.style.padding = '5px';
+                quantityLabel.style.opacity = '0.7';
+                quantityLabel.style.display = 'flex';
+                quantityLabel.style.alignItems = 'center';
+                quantityLabel.style.justifyContent = 'center';
+
+                // Set z-index (mostly for EDHREC) to make sure it's on top of everything
+                //     5 goes behind everything
+                //     6 goes between back and front
+                //     7 goes on top of front but behind back card brought forward
+                //     8 goes on top of everything
+                //     10 should be nice and safe
+                quantityLabel.style.zIndex = '10';
+
+                parentDiv.style.position = 'relative';
+                parentDiv.appendChild(quantityLabel);
+            }
+        };
+
         let processImages = function() {
             let images = document.getElementsByTagName('img');
             // console.log('Found ' + images.length + ' images');
     
-            // Skip images we've already processed
             for (let img of images) {
-                if (processedImages.has(img)) {
-                    continue;
-                }
-                
-                // Find the parent <div> of the <img>, to deal with split cards
-                let parentDiv = img.parentNode;
-                while (parentDiv && parentDiv.nodeName !== 'DIV') {
-                    parentDiv = parentDiv.parentNode;
-                }
-
-                // If the parent <div> contains other processed imgs, skip 
-                // this <img>. This is to deal with split cards and 
-                // partner commanders on EDHREC.
-                if (parentDiv) {
-                    let foundOtherImg = false;
-                    let otherImgs = parentDiv.getElementsByTagName('img');
-
-                    for (let otherImg of otherImgs) {
-                        if (processedImages.has(otherImg)) {
-                            foundOtherImg = true;
-                            break;
-                        }
-                    }
-
-                    if (foundOtherImg) {
-                        continue;
-                    }
-                } else {
-                    // Quick-fix: If we didn't find a parent <div>, just use the <img>'s parent
-                    parentDiv = img.parentNode;
-                }
-
-                // Make a note that we've now processed this <img>
-                processedImages.add(img);
-
-                // Clean up the card name from the image's alt text
-                let cardName = img.alt
-                    .toLowerCase()
-                    .replace(/ [-a-z0-9]+$/, '')  // Replace trailing combinations of numbers, dashes, and letters
-                    .replace('product image for', '')  // Strip out "Product Image for"
-                    .replace('- full art', '')  // Strip out "- Full Art"
-                    .replace(/-\d+$/, '')  // Strip "-0", "-2", etc. from end
-                    .replace(/\(.*?\)/g, '')  // Strip out parentheses
-                    .replace(/\[.*?\]/g, '')  // Strip out square brackets
-                    .trim();
-                
-                let useCardNames = [];
-                if (cardQuantities.hasOwnProperty(cardName)) {
-                    useCardNames.push(cardName);
-                }
-
-                // Faces of split cards -- actually for partner commanders on EDHREC
-                if (cardName.includes('//')) {
-                    for (let splitPart of cardName.split('//')) {
-                        splitPart = splitPart.trim();
-                        if (cardQuantities.hasOwnProperty(splitPart)) {
-                            useCardNames.push(splitPart);
-                        } else {
-                            useCardNames.push(null);
-                        }
-                    }
-                }
-
-                let quantityString = 'Collected: ';
-                if (useCardNames.some(cardName => cardName !== null)) {
-                    quantityString += useCardNames.map(cardName => cardQuantities[cardName] ? cardQuantities[cardName] : 0).join(' // ');
-                } else {
-                    useCardNames = [];
-                }
-                
-                if (useCardNames.length > 0) {
-                    // We have a copy of this card, so add a label to the image
-                    // console.log('HAVE ' + cardName + ' (' + cardQuantities[cardName] + ')');
-                    let quantityLabel = document.createElement('div');
-                    quantityLabel.textContent = quantityString;
-                    quantityLabel.style.position = 'absolute';
-                    quantityLabel.style.right = '0';
-                    quantityLabel.style.top = '50%';
-                    quantityLabel.style.transform = 'translateY(-100%)';
-                    quantityLabel.style.backgroundColor = 'gray';
-                    quantityLabel.style.color = 'white';
-                    quantityLabel.style.fontWeight = 'bold';
-                    quantityLabel.style.padding = '5px';
-                    quantityLabel.style.opacity = '0.7';
-                    quantityLabel.style.display = 'flex';
-                    quantityLabel.style.alignItems = 'center';
-                    quantityLabel.style.justifyContent = 'center';
-
-                    // Set z-index (mostly for EDHREC) to make sure it's on top of everything
-                    //     5 goes behind everything
-                    //     6 goes between back and front
-                    //     7 goes on top of front but behind back card brought forward
-                    //     8 goes on top of everything
-                    //     10 should be nice and safe
-                    quantityLabel.style.zIndex = '10';
-
-                    parentDiv.style.position = 'relative';
-                    parentDiv.appendChild(quantityLabel);
-                }
+                processImage(img);
             }
         };
     
@@ -133,4 +138,25 @@ chrome.storage.local.get('cardQuantities').then(
     
         // Start observing the document with the configured parameters
         observer.observe(document, { childList: true, subtree: true });
+
+        // Create an IntersectionObserver to watch for elements entering the viewport
+        let intersectionObserver = new IntersectionObserver(function(entries) {
+            for (let entry of entries) {
+
+                if (entry.isIntersecting) {
+                    // The entry is intersecting the viewport
+
+                    let imgElements = entry.target.getElementsByTagName('img');
+                    for (let img of imgElements) {
+                        processImage(img);
+                    }
+                }
+            }
+        });
+        
+        // Observe all divs with class=lazy-image__wrapper in the document
+        let divs = document.getElementsByClassName('lazy-image__wrapper');
+        for (let div of divs) {
+            intersectionObserver.observe(div);
+        }
     });
